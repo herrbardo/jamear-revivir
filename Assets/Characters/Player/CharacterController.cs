1722d9fe3a95df4f6dd14ce843369cb0ada34911
Gerardo Laguzzi <laguzzigerardo@gmail.com>
2022-02-12 20:02:52 -0300

    Sistema de movimiento y disparo implementado

---
+++ Assets/Characters/Player/CharacterController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    [SerializeField] float Speed = 4f;
+    [SerializeField] float JumpForce;
+
+    Rigidbody2D rb;
+    Vector2 movement;
+    public Transform pointer;
+    public GameObject bala;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Shoot();
+        Jump();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        rb.MovePosition(rb.position + movement * Speed * Time.fixedDeltaTime);
+    }
+
+    void Jump()
+    {
+        if(Input.GetButtonDown("Jump") /*&& Mathf.Abs(rb.velicity.y < .001f)*/)
+        {
+            rb.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+        }
+    }
+
+    void Shoot()
+    {
+        if (Input.GetButton("Fire1")) //click izquierdo o barra espaciadora salvo que se cambie
+            Instantiate(bala, pointer.position, pointer.rotation); 
+    }
+}
---
+++ Assets/Weapons/Bullet/Bala.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bala : MonoBehaviour
+{
+    public float speed;
+    public int damage;
+    public Rigidbody2D rb;
+    public bool IsFacingRight;
+
+    void Start()
+    {
+        if(IsFacingRight)
+            rb.velocity = transform.right * speed;
+        else
+            rb.velocity = -transform.right * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        if(hitInfo.tag == "Player")
+            return;
+
+        if(hitInfo.tag == "Enemy")
+        {
+            Enemy enemy = hitInfo.GetComponent<Enemy>();
+            if (enemy != null)
+                enemy.TakeDamage(damage);
+
+            Destroy(gameObject);
+        }
+    }
+
+    void OnBecameInvisible()
+    {
+        enabled = false;
+        Destroy(gameObject);
+    }
+}
