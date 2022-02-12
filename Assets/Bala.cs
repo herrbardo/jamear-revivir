9640312b466dd2db35a66b8f0c5084c4701a58b7
Franco Martín Pedroncini <70728444+francopedroncini@users.noreply.github.com>
2022-02-12 17:35:42 -0300

    Add files via upload

---
+++ Assets/Bala.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bala : MonoBehaviour
+{
+    public float speed;
+    public int damage;
+    public Rigidbody2D rb;
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        Enemy enemy = hitInfo.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            enemy.TakeDamage(damage);
+        }
+        Destroy(gameObject);
+    }
+    void OnBecameInvisible()
+    {
+        enabled = false;
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/CharacterController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float Speed = 4f;
+
+    public Rigidbody2D rb;
+    Vector2 movement;
+    public Transform pointer;
+    public GameObject bala;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+       
+        if (Input.GetButton("Fire1")) //click izquierdo o barra espaciadora salvo que se cambie
+        {
+            Instantiate(bala, pointer); 
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * Speed * Time.fixedDeltaTime);
+    }
+   
+}
